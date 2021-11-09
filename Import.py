7399dd282b563c22f3bd92f3f224d8fb19d64ec4
Robert Carruthers <keepitsurreal@gmail.com>
2021-11-09 22:59:23 +0000

    init

---
+++ Import.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 22:53:53 2021
+
+@author: rcpc4
+"""
+
+import os
+import sys
+
+os.chdir('C://Code/Projects/air-quality')
+
+import json
+import requests
+
