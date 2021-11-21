43b54ef39d187c4471f0a46bc314746db5e08898
rcdevhub <68498778+rcdevhub@users.noreply.github.com>
2021-11-21 19:18:11 +0000

    Delete plotsup.cmd

--- plotsup.cmd
+++
@@ -1,7 +0,0 @@
-git add -f "plots/pred-act-reg-feat-training-.png"
-git add -f "plots/pred-act-reg-feat-validation-.png"
-git add -f "plots/resid-reg-feat-training-.png"
-git add -f "plots/resid-reg-feat-validation-.png"
-git add -f "plots/pred-time-reg-feat-validation-2017-03-06-2017-03-09.png"
-git add -f "plots/pred-time-reg-feat-validation-2017-05-01-2017-05-15.png"
-git add -f "plots/pred-time-reg-feat-validation-2017-10-01-2017-11-01.png"
